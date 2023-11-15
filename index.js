0d4a0ee2eeb974cb3ad580141556704289437d93
Hokome <marwan.esseghir@hva.nl>
2023-11-15 17:52:45 +0100

    Morgan logging

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
 app.use(express.json());
+morgan.token('data', (req) => JSON.stringify(req['body']));
+app.use(morgan(
+    ':method :url :status :res[content-length] - :response-time ms :data'));
 
 let persons = [
   {'id': 1, 'name': 'Arto Hellas', 'number': '040-123456'},
