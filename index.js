ad00e146369a7d295d4d21ce29a091566f8f7920
Hokome <marwan.esseghir@hva.nl>
2023-11-15 12:56:50 +0100

    Person deletion

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 app.get('/info', (request, response) => {response.send(getInfo())})
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  persons = persons.filter(p => p.id !== id);
+
+  response.status(204).end();
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)})
