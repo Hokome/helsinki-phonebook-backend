095adc2090f262da6453908e1d15b70885e28fc8
Hokome <marwan.esseghir@hva.nl>
2023-11-15 13:13:35 +0100

    Add person to list

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 let persons = [
   {'id': 1, 'name': 'Arto Hellas', 'number': '040-123456'},
@@ -7,6 +8,11 @@ let persons = [
   {'id': 3, 'name': 'Dan Abramov', 'number': '12-43-234345'},
   {'id': 4, 'name': 'Mary Poppendieck', 'number': '39-23-6423122'}
 ];
+
+const generateId =
+    () => {
+      return Math.floor(Math.random() * 999999);
+    }
 
 const getInfo = () => {
   return `Phonebook has info for ${persons.length} people<br>${
@@ -32,6 +38,24 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(p => p.id !== id);
 
   response.status(204).end();
+})
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!(body.name && body.number)) {
+    response.status(400).json({error: 'name and/or number missing'})
+    return;
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons.push(person);
+
+  response.json(person);
 })
 
 const PORT = 3001;
